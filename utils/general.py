132dee1c52389f9f4da7da9edf12c18bed823da8
Chongju Mai <31859452+maichongju@users.noreply.github.com>
2022-08-08 17:48:03 -0400

    Disable custom logging
    Disable the custom logging setting for yolov5. This enable the ability that the master project take control of the logging.

--- utils/general.py
+++ utils/general.py
@@ -95,7 +95,7 @@ def set_logging(name=None, verbose=VERBOSE):
     log.addHandler(handler)
 
 
-set_logging()  # run before defining LOGGER
+#set_logging()  # run before defining LOGGER
 LOGGER = logging.getLogger("yolov5")  # define globally (used in train.py, val.py, detect.py, etc.)
 for fn in LOGGER.info, LOGGER.warning:
     _fn, fn = fn, lambda x: _fn(emojis(x))  # emoji safe logging
